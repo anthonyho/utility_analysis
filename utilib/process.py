2cb354a96c404d77c620081150b71eb5e6d7983e
Anthony Ho <anthony.hyho@gmail.com>
2017-07-20 16:31:19 -0700

    fix bugs with grouping by multiple levels (> 2)

--- utilib/process.py
+++ utilib/process.py
@@ -45,15 +45,15 @@ def read_bills(file, bill_type='elec', test=True):
 def calendarize(df, group_keys=['keyacctid', 'premiseID'],
                 list_fields=['kWh', 'kWhOn', 'kWhSemi', 'kWhOff']):
     new_df = df.groupby(group_keys).apply(_calendarize_group, list_fields)
-    return new_df.reset_index().drop('level_2', axis=1)
+    return new_df.reset_index(drop=True, level=-1).reset_index()
 
 
-def _calendarize_group(group, list_fields=['kWh', 'kWhOn']):
+def _calendarize_group(group, list_fields):
     expanded_rows = []
     for row in group.itertuples():
         expanded_rows.append(_expand_row(row, list_fields))
     expanded_group = pd.concat(expanded_rows, axis=0, ignore_index=True)
-    calendarized_group = expanded_group.groupby('month').sum().reset_index()
+    calendarized_group = expanded_group.groupby('yr_mo').sum().reset_index()
     return calendarized_group
 
 
@@ -62,25 +62,25 @@ def _expand_row(row, list_fields):
     end_date = row.readDate
     n_months = ((end_date.year - start_date.year) * 12 +
                 end_date.month - start_date.month + 1)
-    list_months = [(start_date + DateOffset(months=i)).strftime('%Y-%m')
-                   for i in range(0, n_months)]
+    list_yr_mo = [(start_date + DateOffset(months=i)).strftime('%Y-%m')
+                  for i in range(0, n_months)]
     days_in_months = _get_days_in_months(start_date, end_date,
-                                         n_months, list_months)
+                                         n_months, list_yr_mo)
     frac = days_in_months / row.readDays
-    expanded_row = pd.DataFrame({'month': list_months})
+    expanded_row = pd.DataFrame({'yr_mo': list_yr_mo})
     for field in list_fields:
         expanded_row[field] = getattr(row, field) * frac
     return expanded_row
 
 
-def _get_days_in_months(start_date, end_date, n_months, list_months):
+def _get_days_in_months(start_date, end_date, n_months, list_yr_mo):
     if n_months == 1:
         days_in_months = np.array([(end_date - start_date).days])
     else:
         days_in_month_1 = ((start_date + MonthEnd()) - start_date).days
         days_in_month_n = (end_date - (end_date - MonthBegin())).days + 1
         days_in_months = [days_in_month_1]
-        for month in list_months[1:-1]:
+        for month in list_yr_mo[1:-1]:
             Y, m = list(map(int, month.split("-")))
             days_in_months.append(calendar.monthrange(Y, m)[1])
         days_in_months.append(days_in_month_n)
