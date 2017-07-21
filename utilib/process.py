1adbe801248953bbac3726e81476835fe14669c6
Anthony Ho <anthony.hyho@gmail.com>
2017-07-21 11:14:02 -0700

    add read_costar() function

--- utilib/process.py
+++ utilib/process.py
@@ -14,6 +14,45 @@ import calendar
 # 2. allow calendarizing gas bills
 
 
+def read_costar(file, test=False):
+    usecols = ['PropertyID', 'PropertyType', 'Secondary Type',
+               'Building Address', 'City', 'Zip', 'County Name',
+               'Building Status', 'Year Built', 'Year Renovated',
+               'Number Of Stories', 'Rentable Building Area',
+               'Energy Star', 'LEED Certified', 'Last Sale Date', 'Vacancy %']
+    dtype = {'PropertyID': str,
+             'PropertyType': str,
+             'Secondary Type': str,
+             'Building Address': str,
+             'City': str,
+             'Zip': str,
+             'County Name': str,
+             'Building Status': str,
+             'Year Built': np.float16,
+             'Year Renovated': np.float16,
+             'Number Of Stories': np.float16,
+             'Rentable Building Area': np.float16,
+             'Energy Star': str,
+             'LEED Certified': str,
+             'Last Sale Date': str,
+             'Vacancy %': np.float16}
+    encoding = 'iso-8859-1'
+    engine = 'c'
+    nrows = 10000 if test else None
+
+    data = pd.read_csv(file,
+                       usecols=usecols, dtype=dtype,
+                       encoding=encoding, engine=engine,
+                       nrows=nrows)
+
+    data['Building Address'] = data['Building Address'].str.upper()
+    data['City'] = data['City'].str.upper()
+    data['Zip'] = data['Zip'].str[:5]
+    data['County Name'] = data['County Name'].str.upper()
+
+    return data
+
+
 def read_bills(file, bill_type='elec', test=True):
     usecols = ['keyacctid', 'premiseID', 'rate',
                'readDate', 'lastReadDate', 'readDays',
