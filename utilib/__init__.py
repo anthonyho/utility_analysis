60a98f6448ced3cf900495be4a7e9b4bb8979a7a
Anthony Ho <anthony.hyho@gmail.com>
2017-08-01 14:05:14 -0700

    add plot submodule and setproperties() and plot_heatmap() functions

--- utilib/__init__.py
+++ utilib/__init__.py
@@ -1,2 +1,4 @@
 from utilib import process
 from utilib import read
+from utilib import miscel
+from utilib import plot
---
+++ utilib/plot.py
@@ -0,0 +1,161 @@
+# Anthony Ho <anthony.ho@energy.ca.gov>
+# Last update 8/1/2017
+"""
+Python module for plotting utility data
+"""
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Handy function for setting commonly used plot modifiers
+def setproperties(fig=None, ax=None, figsize=None,
+                  suptitle=None, title=None,
+                  legend=None, legendloc=1, legendwidth=2.5, legendbox=None,
+                  xlabel=None, ylabel=None, xlim=None, ylim=None,
+                  scix=False, sciy=False,
+                  scilimitsx=(-3, 3), scilimitsy=(-3, 3),
+                  logx=False, logy=False, majorgrid=None, minorgrid=None,
+                  borderwidth=2.5, tight=True, pad=1.6,
+                  fontsize=None, legendfontsize=20, tickfontsize=20,
+                  labelfontsize=20, titlefontsize=18, suptitlefontsize=20,
+                  xticklabelrot=None, yticklabelrot=None,
+                  equal=False, symmetric=False):
+    """Convenient tool to set properties of a plot in a single command"""
+    # Get figure and axis handles
+    if not fig:
+        fig = plt.gcf()
+    if not ax:
+        ax = plt.gca()
+
+    # Set background color to white
+    fig.patch.set_facecolor('w')
+
+    # Define figure size if provided
+    if figsize:
+        fig.set_size_inches(figsize, forward=True)
+
+    # Set titles if provided
+    if suptitle is not None:
+        if fontsize is None:
+            fig.suptitle(suptitle, fontsize=suptitlefontsize, y=0.99)
+        else:
+            fig.suptitle(suptitle, fontsize=fontsize, y=0.99)
+    if title is not None:
+        ax.set_title(title, y=1.02)
+    # Show legend if requested
+    if legend:
+        legend = plt.legend(loc=legendloc, numpoints=1,
+                            fontsize=legendfontsize, frameon=legendbox)
+        legend.get_frame().set_linewidth(legendwidth)
+    # Set x and y labels if provided
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
+    # Set x and y limits if provided
+    if xlim is not None:
+        ax.set_xlim(xlim)
+    if ylim is not None:
+        ax.set_ylim(ylim)
+    # Apply scientific notation to x and y tick marks if requested
+    if scix:
+        ax.ticklabel_format(axis='x', style='sci', scilimits=scilimitsx)
+    if sciy:
+        ax.ticklabel_format(axis='y', style='sci', scilimits=scilimitsy)
+    # Change axis to log scale if requested
+    if logx:
+        ax.set_xscale('log')
+    if logy:
+        ax.set_yscale('log')
+    # Set major and minor grid in plot
+    if majorgrid is not None:
+        ax.grid(b=majorgrid, which='major')
+    if minorgrid is not None:
+        ax.grid(b=minorgrid, which='minor')
+
+    # Rotate x and y tick labels if requested
+    if xticklabelrot is not None:
+        xticklabels = ax.get_xticklabels()
+        for ticklabel in xticklabels:
+            ticklabel.set_rotation(xticklabelrot)
+    if yticklabelrot is not None:
+        yticklabels = ax.get_yticklabels()
+        for ticklabel in yticklabels:
+            ticklabel.set_rotation(yticklabelrot)
+
+    # Set borderwidth (not visible if using seaborn default theme)
+    for axis in ['top', 'bottom', 'left', 'right']:
+        ax.spines[axis].set_linewidth(borderwidth)
+
+    # Set individual fontsizes if fontsize is not specified
+    if fontsize is None:
+        plt.setp(ax.get_xticklabels(), fontsize=tickfontsize)
+        plt.setp(ax.get_yticklabels(), fontsize=tickfontsize)
+        ax.xaxis.label.set_fontsize(labelfontsize)
+        ax.yaxis.label.set_fontsize(labelfontsize)
+        ax.title.set_fontsize(titlefontsize)
+    # Set all fontsizes to fontsize if fontsize is specified
+    else:
+        plt.setp(ax.get_xticklabels(), fontsize=fontsize)
+        plt.setp(ax.get_yticklabels(), fontsize=fontsize)
+        ax.xaxis.label.set_fontsize(fontsize)
+        ax.yaxis.label.set_fontsize(fontsize)
+        ax.title.set_fontsize(fontsize)
+
+    # Set tight figure and padding
+    if tight:
+        fig.tight_layout(pad=pad)
+
+    # Set equal aspect
+    if equal:
+        ax.set_aspect('equal', adjustable='box')
+
+    # Set symmetric axis limits
+    if symmetric:
+        xlim_abs = max(abs(i) for i in ax.get_xlim())
+        ylim_abs = max(abs(i) for i in ax.get_ylim())
+        ax.set_xlim((-xlim_abs, xlim_abs))
+        ax.set_ylim((-ylim_abs, ylim_abs))
+
+
+def plot_heatmap_type_cz(df, types, cz, value):
+    # Extract and process data
+    ind = pd.Series([False] * len(df))
+    for type_tuple in types:
+        property_type = type_tuple[0]
+        secondary_type = type_tuple[1]
+        in_type = ((df[('cis', 'PropertyType')] == property_type) &
+                   (df[('cis', 'Secondary Type')] == secondary_type))
+        ind = ind | in_type
+
+    cz = [str(i) for i in cz]
+    ind = ind & df[('cis', 'cz')].isin(cz)
+
+    # Extract
+    data = df.loc[ind,
+                  [('cis', 'cz'),
+                   ('cis', 'PropertyType'),
+                   ('cis', 'Secondary Type'),
+                   value]
+                  ]
+
+    # Process
+    data.columns = data.columns.droplevel()
+    data['Property type'] = data['PropertyType'] + ' - ' + data['Secondary Type']
+    data = data.drop('PropertyType', axis=1)
+    data = data.drop('Secondary Type', axis=1)
+    data['cz'] = data['cz'].astype(int)
+
+    # Compute
+    summary = data.groupby(['Property type', 'cz']).mean().reset_index()
+
+    # Reshape
+    summary = summary.pivot(index='Property type',
+                            columns='cz',
+                            values=value[1])
+
+    return summary
